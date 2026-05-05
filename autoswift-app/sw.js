23b9969784ad5645a4eb184ea28e4670ade6ca70
msaleeth75-arch <msaleeth75@gmail.com>
2026-05-05 09:47:00 +0400

    Delete autoswift-app directory

--- autoswift-app/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('[Service Worker] Installed');
-});
-
-self.addEventListener('fetch', (event) => {
-    // Simply pass through requests for now. 
-    // In a full PWA, we would cache files here for offline support.
-    event.respondWith(fetch(event.request));
-});
